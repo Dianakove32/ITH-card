65b88a6dcbdbfba64a3a6e919ab0d18a0c1c9153
Dziana Kavaleva <dianakove32@gmail.com>
2021-08-30 18:57:08 +0300

    work on comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,47 @@
-import { Card, CardContent, makeStyles, Typography } from '@material-ui/core';
-import './App.scss';
+import "./App.scss";
+import { v4 as uuidv4 } from "uuid";
 
-interface dataTS {
-    title: string,
-    description: string,
+interface CardsDataDTO {
+    title: string;
+    description: string;
+    id: string;
 }
 
-let data: dataTS[] = [
-  {
-      title: 'Spain',
-      description: 'is a country in Southwestern Europe with some pockets of territory in the Mediterranean Sea, offshore in the Atlantic Ocean and across the Strait of Gibraltar.',
-  },
-  {
-      title: 'Egypt',
-      description: 'officially the Arab Republic of Egypt, is a transcontinental country spanning the northeast corner of Africa and southwest corner of Asia by a land bridge formed by the Sinai Peninsula.',
-  },
-  {
-      title: 'France',
-      description: 'is a transcontinental country spanning Western Europe and several overseas regions and territories.',
-  },
-]
-
-const useStyles = makeStyles({
-    root: {
-      maxWidth: 300,
+let data: CardsDataDTO[] = [
+    {
+        title: "Spain",
+        description:
+        "Is a country in Southwestern Europe with some pockets of territory in the Mediterranean Sea, offshore in the Atlantic Ocean and across the Strait of Gibraltar.",
+        id: uuidv4(),
     },
-    media: {
-      height: 140,
+    {
+        title: "Egypt",
+        description:
+        "Officially the Arab Republic of Egypt, is a transcontinental country spanning the northeast corner of Africa and southwest corner of Asia by a land bridge formed by the Sinai Peninsula.",
+        id: uuidv4(),
     },
-  });
+    {
+        title: "France",
+        description:
+        "Is a transcontinental country spanning Western Europe and several overseas regions and territories.",
+        id: uuidv4(),
+    },
+];
 
 const App = () => {
-  const classes = useStyles();
-
-  return (
-      <div className = 'App'>
-      <h1>itechart-react-course</h1>
-          <div className = 'cards-container'>
-          {
-              data.map((el, i) =>
-                  <Card className={classes.root} key = {i}>
-                      <CardContent>
-                          <Typography gutterBottom variant="h5" component="h2">
-                              {el.title}
-                          </Typography>
-                          <Typography variant="body2" color="textSecondary" component="p">
-                              {el.description}
-                          </Typography>
-                      </CardContent>
-                  </Card>)
-          }
-          </div>
-      </div>
-  )
-}
+    return (
+        <div className="App">
+        <h1>itechart-react-course</h1>
+        <div className="cards-container">
+            {data.map((el) => (
+            <div className="card" key={el.id}>
+                <h3>{el.title}</h3>
+                <p>{el.description}</p>
+            </div>
+            ))}
+        </div>
+        </div>
+    );
+};
 
 export default App;
